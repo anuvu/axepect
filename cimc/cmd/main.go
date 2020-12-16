e7c0a8553da3d2fc56a40b3b564d537d47d93ab8
Scott Moser <smoser@brickies.net>
2020-12-16 16:49:56 -0500

    Add better cli to cimc/cmd/demo.

--- cimc/cmd/main.go
+++ cimc/cmd/main.go
@@ -2,14 +2,58 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
+	"strings"
 
 	"github.com/anuvu/axepect/cimc"
 	"github.com/anuvu/axepect/loginshell"
+	"github.com/apex/log"
+	"github.com/urfave/cli/v2"
 )
 
+var version string
+
 func main() {
-	cs, err := cimc.NewSession("10.99.99.99", "admin", "FIXMEPLEASE")
+	app := &cli.App{
+		Name:    "cimc",
+		Version: version,
+		Usage:   "Play around with cimc",
+		Commands: []*cli.Command{
+			&cli.Command{
+				Name:   "demo",
+				Usage:  "demo the cimc stuff",
+				Action: demoMain,
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "serial-login",
+						Usage: "Attempt serial login over SOL with user:pass",
+					},
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+}
+
+func demoMain(c *cli.Context) error {
+	if c.Args().Len() != 1 {
+		return fmt.Errorf("Got %d args, expected 1 (user:pass@ip)", c.Args().Len())
+	}
+
+	userHostPass := c.Args().First()
+	toks := strings.SplitN(userHostPass, "@", 2)
+	host := toks[1]
+	toks = strings.SplitN(toks[0], ":", 2)
+	user := toks[0]
+	pass := toks[1]
+
+	loginCreds := c.String("serial-login")
+
+	cs, err := cimc.NewSession(host+":22", user, pass)
 	if err != nil {
 		log.Fatalf("failed new session: %v", err)
 	}
@@ -25,28 +69,37 @@ func main() {
 		fmt.Printf("%s", ret)
 	}
 
-	exp, err := cs.ExpectHostConsole()
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-	exp.Send("\n\n")
+	if loginCreds != "" {
+		toks := strings.SplitN(loginCreds, ":", 2)
+		loginUser := toks[0]
+		loginPass := toks[1]
 
-	rshell, err := loginshell.Login(exp, "rescue-user", "FIXMEPLEASE")
-	if err != nil {
-		log.Fatalf("Failed to login: %v\n", err)
-	}
+		exp, err := cs.ExpectHostConsole()
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
 
-	cmdR := rshell.Run("acs debug-token\n")
-	if cmdR.RC != 0 {
-		log.Fatalf("Failed to send command: \n%s\n", cmdR.String())
-	}
+		log.Info(fmt.Sprintf("Connected to host console, attempting login as '%s'", loginUser))
+		exp.Send("\n\n")
 
-	if err := rshell.Logout(); err != nil {
-		log.Fatalf("Failed to logout: %v\n", err)
-	}
+		rshell, err := loginshell.Login(exp, loginUser, loginPass)
+		if err != nil {
+			log.Fatalf("Failed to login: %v\n", err)
+		}
 
-	if err := cs.EndHostConsole(); err != nil {
-		log.Fatalf("Failed to exit host console\n")
+		cmdR := rshell.Run("acs debug-token\n")
+		if cmdR.RC != 0 {
+			log.Fatalf("Failed to send command: \n%s\n", cmdR.String())
+		}
+
+		if err := rshell.Logout(); err != nil {
+			log.Fatalf("Failed to logout: %v\n", err)
+		}
+
+		if err := cs.EndHostConsole(); err != nil {
+			log.Fatalf("Failed to exit host console\n")
+		}
+
 	}
 
 	if out, err := cs.SendCmd("/show version"); err != nil {
@@ -60,4 +113,6 @@ func main() {
 	}
 
 	fmt.Printf("All done\n")
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/google/goexpect v0.0.0-20200816234442-b5b77125c2c5
 	github.com/google/goterm v0.0.0-20190703233501-fc88cf888a3f
 	github.com/pkg/errors v0.9.1
+	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
 )
