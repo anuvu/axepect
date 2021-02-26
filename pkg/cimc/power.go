3427eedd3f66ca5c1e1c14c7b983abc900a85ff2
Scott Moser <smoser@brickies.net>
2021-02-25 22:53:33 -0500

    Fix PowerCycle to turn on if system is already off.
    If you send 'power cycle' to a cimc where the system is currently
    off, it will do nothing.
    
    This fixes PowerCycle to do what the user wanted...
     * if the system is off, turn it on.
     * if the system is on, power cycle.

--- pkg/cimc/power.go
+++ pkg/cimc/power.go
@@ -37,7 +37,16 @@ func (cs *Session) PowerOn(ctx context.Context) error {
 
 // PowerCycle - Turn power off, if off and then back on.
 func (cs *Session) PowerCycle(ctx context.Context) error {
-	return powerCmd(ctx, cs, "cycle")
+	curState, err := cs.GetPowerState(ctx)
+	if err != nil {
+		return err
+	}
+	operation := "cycle"
+	if curState == Off {
+		// system was off, turn it on.
+		operation = "on"
+	}
+	return powerCmd(ctx, cs, operation)
 }
 
 func powerCmd(ctx context.Context, cs *Session, cmd string) error {
